4484aac8c5724b78b36fbd16166de0ed2838db1a
Kanchan <info.kanchankumar@gmail.com>
2023-03-12 18:14:30 +0530

    Added CRUD

--- h2-database/src/main/java/com/h2database/H2DatabaseApplication.java
+++ h2-database/src/main/java/com/h2database/H2DatabaseApplication.java
@@ -5,21 +5,57 @@ import com.h2database.entities.User;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
 
 import java.time.LocalDate;
 import java.time.Month;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
+
 @SpringBootApplication
+@ComponentScan({"com.h2database.dao", "com.h2database.entities"})
 public class H2DatabaseApplication {
     public static void main(String[] args) {
 
         ConfigurableApplicationContext context = SpringApplication.run(H2DatabaseApplication.class, args);
         UserRepository userRepository = context.getBean(UserRepository.class);
 
-        createUser(userRepository);
+        //CRUD - Create, Read, Update and Delete
         createUsers(userRepository);
+        findUserById(userRepository);
+        findAllUsers(userRepository);
+        updateUserAgeById(userRepository);
+        deleteUserById(userRepository);
     }
+
+    private static void deleteUserById(UserRepository userRepository) {
+        int userId = 2;
+        System.out.println("Deleting the User where id is : " + userId);
+        userRepository.deleteById(userId);
+    }
+
+    private static void updateUserAgeById(UserRepository userRepository) {
+        int userId = 1;
+        int newAge = 35;
+        System.out.println("Finding Entities By ID " + userId);
+        Optional<User> findById = userRepository.findById(userId);
+        User user = findById.get();
+        user.setAge(newAge);
+        userRepository.save(user);
+    }
+
+    private static void findAllUsers(UserRepository userRepository) {
+        userRepository.findAll().forEach(System.out::println);
+    }
+
+    private static void findUserById(UserRepository userRepository) {
+        int userId = 2;
+        Optional<User> findById = userRepository.findById(userId);
+        User user = findById.get();
+        System.out.println(user);
+    }
+
     private static void createUsers(UserRepository userRepository) {
         User kk = new User();
         kk.setName("KANCHAN KUMAR");
@@ -37,14 +73,5 @@ public class H2DatabaseApplication {
         Iterable<User> dbSaveUser = userRepository.saveAll(userList);
         System.out.println("Following User Save into DB");
         dbSaveUser.forEach(System.out::println);
-    }
-    private static void createUser(UserRepository userRepository) {
-        User user = new User();
-        user.setName("KANCHAN");
-        user.setAge(30);
-        user.setDob(LocalDate.of(1990, Month.DECEMBER, 16));
-        User dbUser = userRepository.save(user);
-        System.out.println("Following User Save into DB");
-        System.out.println(dbUser);
     }
 }
